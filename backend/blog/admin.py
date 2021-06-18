c02a8025bfa27f3acdd7eb36569e705d555838fa
mike <vickers.mike@gmail.com>
2021-06-18 07:35:25 -0400

    first commit

---
+++ backend/blog/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from .models import Profile, Post, Tag
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = (
+        'id',
+        'title',
+        'subtitle',
+        'slug',
+        'published_date',
+        'published',
+    )
+    list_filter = (
+        'published',
+        'published_date',
+    )
+    list_editable = (
+        'title',
+        'subtitle',
+        'slug',
+        'published_date',
+        'published',
+    )
+    search_fields = (
+        'title',
+        'subtitle',
+        'slug',
+        'body'
+    )
+    prepopulated_fields = {
+        'slug': (
+            'title',
+            'subtitle',
+        )
+    }
+    date_hierarchy = 'published_date'
+    save_on_top = True
